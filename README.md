3523dd8bc2097994c6ffe3a579636ed1a4e41ab7
André König <andre.koenig@gmail.com>
2014-04-17 23:22:48 +0200

    Initial module structure.

---
+++ README.md
@@ -0,0 +1,15 @@
+# npmawesome-twitter-bot
+
+The bot which tweets #npmawesome posts.
+
+## Installation
+
+Install with [npm](https://npmjs.org/package/npmawesome-twitter-bot).
+
+    npm install --save npmawesome-twitter-bot
+
+## Changelog
+
+## Author
+
+Copyright 2014, [André König](http://) (andre.koenig@posteo.de)
---
+++ specs/index.spec.js
@@ -0,0 +1,21 @@
+/*
+ * npmawesome-twitter-bot
+ *
+ * Copyright(c) 2014 André König <andre.koenig@posteo.de>
+ * MIT Licensed
+ *
+ */
+
+/**
+ * @author André König <andre.koenig@posteo.de>
+ *
+ */
+
+'use strict';
+
+describe('The "npmawesome-twitter-bot"', function () {
+
+    it('should ...', function (done) {
+        done();
+    });
+});
