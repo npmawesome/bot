9ac3abb1d706ad0feea69d12cbedeffbc8fcfeb5
André König <andre.koenig@gmail.com>
2014-04-18 18:01:10 +0200

    Added version arg (#cli).

--- cli/index.js
+++ cli/index.js
@@ -19,22 +19,24 @@
  * Module dependencies
  *
  */
-var argv = require('yargs');
+var yargs = require('yargs');
+var pkg = require('../package.json');
 
 var Bot = require('../');
 var utilities = require('./utilities');
 var rc = require('./rc');
 
-argv = argv
+var argv = yargs
     .usage('A Twitter bot and realtime consumer for #npmawesome.\n')
     .example('npmawesome-bot --watch', '"Realtime" #npmawesome updates.')
     .alias('q', 'quiet')
     .alias('d', 'dry-run')
     .alias('w', 'watch')
-    .demand('watch')
+    .alias('v', 'version')
     .describe('watch', 'You will see new #npmawesome posts as they arrive!')
     .describe('quiet', 'No stdout output.')
     .describe('dry-run', 'No fear, no Twitter interaction in this mode. :)')
+    .describe('version', 'Version information')
     .argv;
 
 var bot;
@@ -43,6 +45,18 @@ function onTweet (err, tweet) {
     if (err) {
         return utilities.printError(err.message);
     }
+}
+
+if (argv.version) {
+    console.log(pkg.version);
+
+    return process.exit(0);
+}
+
+if (!argv.watch) {
+    yargs.showHelp();
+
+    return process.exit(1);
 }
 
 // 1. Step: Let's check if the configuration file is available.
