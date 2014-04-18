6b35f22d947687b056c02cf5d071cc0b0458ea8d
André König <andre.koenig@gmail.com>
2014-04-18 12:06:36 +0200

    Finalized the Emitter and implemented startup file.

--- index.js
+++ index.js
@@ -12,3 +12,36 @@
  */
 
 'use strict';
+
+var pkg = require('./package.json');
+var Feed = require('./lib/').Feed;
+
+function Bot (options) {
+    this.name = pkg.name;
+    this.version = pkg.version;
+
+    this.$$feed = Feed.create(options.feed);
+
+    this.$$twitter = null;
+}
+
+/**
+ * DOCME
+ *
+ * @return {[type]} [description]
+ *
+ */
+Bot.prototype.watch = function watch () {
+    return this.$$feed.watch();
+};
+
+/**
+ * DOCME
+ *
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ *
+ */
+exports.create = function create (options) {
+    return new Bot(options);
+};
